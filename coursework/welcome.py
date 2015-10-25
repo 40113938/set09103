f037bcb080d04bbf7429e676f2cae03025c0bb4c
40113938 <richard_cook@hotmail.co.uk>
2015-10-25 17:17:58 +0000

    Basic page nearly done - inputs

--- coursework/welcome.py
+++ coursework/welcome.py
@@ -7,14 +7,20 @@ def welcome():
     print request.form
     name = request.form['name']
     return "Hello %s" % name
+    print request.form
+    password=requrest.form['password']
   else:
     page='''
     <html><body>
       <form action = "" method="post" name="form">
+        <h1> Music Database </h1>
         <label for="name">Name:</label>
         <input type="text" name="name" id="name"/>
-        <input type="submit" name="submit" id="submit"/>
+        <label for="password">Password::</label>
+        <input type="password" name="password" id="password"/>
+        <input type="submit" name="submit" id="submit"/>        
       </form>
+      <h3> Richard Cook - 40113938 </h3>
       </body><html>'''
 
     return page
