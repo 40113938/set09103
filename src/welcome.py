9545eba49424e022eb4fa883591743627885a9f0
40113938 <richard_cook@hotmail.co.uk>
2015-10-27 14:37:52 +0000

    Now displays artists

--- src/welcome.py
+++ src/welcome.py
@@ -35,7 +35,7 @@ def artist():
     cur = g.db.execute ('SELECT * from artist where name = ?', (band, ))
   artist = [dict(name=row[0],
   yoc=row[1], nationality=row[2], genre=row[3]) for row in cur.fetchall()]
-  return render_template('artit.html', band=band, artist=artist)
+  return render_template('artist.html', band=band, artist=artist)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
