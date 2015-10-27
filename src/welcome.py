f26797264cdfc22aaecee893d8fd0740060ca9a4
40113938 <richard_cook@hotmail.co.uk>
2015-10-27 15:57:03 +0000

    Made album page and edited links

--- src/welcome.py
+++ src/welcome.py
@@ -37,5 +37,17 @@ def artist():
   yoc=row[1], nationality=row[2], genre=row[3]) for row in cur.fetchall()]
   return render_template('artist.html', band=band, artist=artist)
 
+@app.route("/album/", methods=['POST', 'GET'])
+def album():
+  band = request.args.get('q', '')
+  g.db = get_db()
+  if band == '':
+    cur = g.db.execute ('SELECT * FROM album ORDER BY name')
+  else:
+    cur.g.db.execute ('SELECT * FROM album where name = ?', (band, ))
+  album = [dict(name=row[0],
+  artist=row[1], yor=row[2], length=row[3]) for row in cur.fetchall()]
+  return render_template('album.html', band=band, album=album)
+
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
